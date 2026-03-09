c17db04c13e8941a69ff1c222114c1cc0ae08267
Fabian Palmera <fabipal.106@gmail.com>
2026-03-09 17:35:58 -0400

    add habitwealth with translate

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,15 @@
+
 <template>
   <div>
     <UPageHero
-      title="Nuxt Starter Template"
-      description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
+      title="HabitWealth"
+      :description="$t('hero_description')"
       :links="[{
-        label: 'Get started',
+        label: $t('Get started'),
         to: 'https://ui.nuxt.com/docs/getting-started/installation/nuxt',
         target: '_blank',
         trailingIcon: 'i-lucide-arrow-right',
         size: 'xl'
-      }, {
-        label: 'Use this template',
-        to: 'https://github.com/nuxt-ui-templates/starter',
-        target: '_blank',
-        icon: 'i-simple-icons-github',
-        size: 'xl',
-        color: 'neutral',
-        variant: 'subtle'
       }]"
     />
 
